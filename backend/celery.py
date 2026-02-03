6d1e686808f878f9b2c020abd8382f750c477b77
arj24092003 <arjunjagadish249@gmail.com>
2026-02-03 15:08:37 +0530

    second commit

--- backend/celery.py
+++ backend/celery.py
@@ -17,8 +17,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 app = Celery('backend')
 
 # Use Redis as broker and backend
-app.conf.broker_url = 'redis://127.0.0.1:6379/0'
-app.conf.result_backend = 'redis://127.0.0.1:6379/1'
+app.conf.broker_url = 'redis://redis:6379/0'
+app.conf.result_backend = 'redis://redis:6379/1'
 
 # Optional config
 app.conf.task_serializer = 'json'
